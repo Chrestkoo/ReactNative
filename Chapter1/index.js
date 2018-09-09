995a1f05f7006de502298a40931f9ca662e1521d
Chrestkoo <sakaiyuji93@hotmail.com>
2018-09-10 02:46:27 +0800

    chapter 1

--- Chapter1/index.js
+++ Chapter1/index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './scr/MainApp';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
